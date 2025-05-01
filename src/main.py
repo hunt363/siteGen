09c654f8c61d065f316f72f245f100086506f6d2
hunt363 <kinchitkumar306@gmail.com>
2025-05-01 08:24:10 +0000

    1.0 done

--- src/main.py
+++ src/main.py
@@ -1,17 +1,47 @@
+from block_nodes import markdown_to_html_node
 from textnode import TextNode
 import os
 import shutil
 
 
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("No title found in markdown")
+
+
+def generate_page(from_path: str, template_path: str, dest_path: str):
+    markdown = open(from_path, "r").read()
+    template = open(template_path, "r").read()
+    html = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+    template = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    open(dest_path, "w").write(template)
+
+
+def generate_pages_recursive(
+    dir_path_content: str, template_path: str, dest_dir_path: str
+):
+    if not os.path.exists(dest_dir_path):
+        os.makedirs(dest_dir_path)
+    for item in os.listdir(dir_path_content):
+        item_path = os.path.join(dir_path_content, item)
+        if item.endswith(".md"):
+            dest_file_path = os.path.join(dest_dir_path, item.replace(".md", ".html"))
+            generate_page(item_path, template_path, dest_file_path)
+        elif os.path.isdir(item_path):
+            new_dir_path = os.path.join(dest_dir_path, item)
+            generate_pages_recursive(item_path, template_path, new_dir_path)
+
+
 def copy_static_files(src: str, dst: str):
-    # Step 1: If destination exists, delete it
     if os.path.exists(dst):
         shutil.rmtree(dst)
 
-    # Step 2: Recreate the destination directory
     os.mkdir(dst)
 
-    # Step 3: Recursive copy
     for item in os.listdir(src):
         src_path = os.path.join(src, item)
         dst_path = os.path.join(dst, item)
@@ -20,7 +50,6 @@ def copy_static_files(src: str, dst: str):
             shutil.copy(src_path, dst_path)
             print(f"Copied file: {src_path} -> {dst_path}")
         elif os.path.isdir(src_path):
-            # Create directory then recursively copy
             os.mkdir(dst_path)
             print(f"Created directory: {dst_path}")
             copy_static_files(src_path, dst_path)
@@ -28,6 +57,7 @@ def copy_static_files(src: str, dst: str):
 
 def main():
     copy_static_files("static", "public")
+    generate_pages_recursive("content", "template.html", "public")
 
 
 if __name__ == "__main__":
